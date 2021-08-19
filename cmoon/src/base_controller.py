6b30687a8227c0ed47778d55f4caf630209e8d26
Cmoon <88758720+Cmoon-cyl@users.noreply.github.com>
2021-08-19 17:27:17 +0800

    Add files via upload

---
+++ cmoon/src/base_controller.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: UTF-8 
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class Base:
+    def __init__(self):
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.twist = Twist()
+
+    def rotate(self, speed):
+        self.twist.linear.x = 0
+        self.twist.linear.y = 0
+        self.twist.linear.z = 0
+        self.twist.angular.x = 0
+        self.twist.angular.y = 0
+        self.twist.angular.z = speed
+        self.pub.publish(self.twist)
+
+    def stop(self):
+        self.twist.linear.x = 0
+        self.twist.linear.y = 0
+        self.twist.linear.z = 0
+        self.twist.angular.x = 0
+        self.twist.angular.y = 0
+        self.twist.angular.z = 0
+        self.pub.publish(self.twist)
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('base', anonymous=True)
+        Base('name')
+    except rospy.ROSInterruptException:
+        pass

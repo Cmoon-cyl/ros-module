282a5c8ca10b2c94e0562fc881899de06fde708f
Cmoon <88758720+Cmoon-cyl@users.noreply.github.com>
2021-08-20 15:37:07 +0800

    Add files via upload

--- cmoon/src/soundplayer.py
+++ cmoon/src/soundplayer.py
@@ -8,19 +8,20 @@ from sound_play.libsoundplay import SoundClient  # soundplay功能包
 
 class Soundplayer:
     def __init__(self):
-        self.soundhandle = SoundClient()  #初始化
+        self.soundhandle = SoundClient()  # 初始化
         rospy.loginfo('Ready to play sound...')
 
-    def say(self, string):
+    def say(self, string, delay=2):
         rospy.sleep(1)
         self.soundhandle.stopAll()  # 停止其他在说的话,保证一次说一句
-        self.soundhandle.say(string)  #说传入的字符串
-        rospy.sleep(2)
+        self.soundhandle.say(string)  # 说传入的字符串
+        rospy.sleep(delay)
 
     def play(self, string):
         rospy.sleep(1)
         self.soundhandle.stopAll()  # 停止其他在说的话,保证一次说一句
         self.soundhandle.say(string)  # 说传入的字符串
+
 
 if __name__ == '__main__':
     try:

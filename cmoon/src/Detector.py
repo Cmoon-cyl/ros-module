da683fed717293256ca003222daaf22ccde98339
Cmoon <88758720+Cmoon-cyl@users.noreply.github.com>
2021-10-09 10:18:25 +0800

    Add files via upload

--- cmoon/src/Detector.py
+++ cmoon/src/Detector.py
@@ -7,6 +7,7 @@ import cv2
 from aip import AipBodyAnalysis, AipFace
 import base64
 import os
+from pyKinectAzure import pyKinectAzure, _k4a
 
 
 class Detector(object):
@@ -17,22 +18,74 @@ class Detector(object):
         with open(filePath, 'rb') as fp:
             return fp.read()
 
-    def take_photo(self):
-        """电脑摄像头拍照保存"""
-        cap = cv2.VideoCapture(2, cv2.CAP_DSHOW)
-        cap.open(0)
-        flag, frame = cap.read()
+    def take_k4a_photo(self):
+        self.modulePath = r'/usr/lib/x86_64-linux-gnu/libk4a.so'
+        self.k4a = pyKinectAzure(self.modulePath)
+        self.k4a.device_open()
+        device_config = self.k4a.config
+        device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_1080P
+        print(device_config)
+        self.k4a.device_start_cameras(device_config)
         path = self.photopath + '/photo.jpg'
-        cv2.imwrite(path, frame)
-        cap.release()
+        while True:
+            self.k4a.device_get_capture()
+            color_image_handle = self.k4a.capture_get_color_image()
+            if color_image_handle:
+                color_image = self.k4a.image_convert_to_numpy(color_image_handle)
+                cv2.imwrite(path, color_image)
+                if 'photo.jpg' in os.listdir(self.photopath):
+                    self.k4a.image_release(color_image_handle)
+                    self.k4a.capture_release()
+                    break
+        self.k4a.device_stop_cameras()
+        self.k4a.device_close()
+        return path
+
+    def take_photo(self, device='camera'):
+        """电脑摄像头拍照保存"""
+        if device == 'k4a':
+            self.modulePath = r'/usr/lib/x86_64-linux-gnu/libk4a.so'
+            self.k4a = pyKinectAzure(self.modulePath)
+            self.k4a.device_open()
+            device_config = self.k4a.config
+            device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_1080P
+            print(device_config)
+            self.k4a.device_start_cameras(device_config)
+            path = self.photopath + '/photo.jpg'
+            while True:
+                self.k4a.device_get_capture()
+                color_image_handle = self.k4a.capture_get_color_image()
+                if color_image_handle:
+                    color_image = self.k4a.image_convert_to_numpy(color_image_handle)
+                    cv2.imwrite(path, color_image)
+                    if 'photo.jpg' in os.listdir(self.photopath):
+                        self.k4a.image_release(color_image_handle)
+                        self.k4a.capture_release()
+                        break
+            self.k4a.device_stop_cameras()
+            self.k4a.device_close()
+        else:
+            cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+            cap.open(0)
+            flag, frame = cap.read()
+            path = self.photopath + '/photo.jpg'
+            cv2.imwrite(path, frame)
+            cap.release()
         return path
 
     def get_attr(self, *key):
         return
 
-    def detect(self, attributes=None, *keys):
+    def detect(self, attributes=None, device='camera', *keys):
         """电脑摄像头拍照检测"""
-        path = self.take_photo()
+        path = self.take_photo(device)
+        print(path)
+        result = self.get_attr(path, attributes)
+        return result
+
+    def k4a_detect(self, attributes=None, *keys):
+        """电脑摄像头拍照检测"""
+        path = self.take_k4a_photo()
         print(path)
         result = self.get_attr(path, attributes)
         return result
@@ -58,7 +111,6 @@ class BodyDetector(Detector):
         for items in attributes:
             attr = attr + items + ','
         image = self.get_file_content(path)
-        self.client.bodyAttr(image)
         options = {}
         # options["type"] = "gender,age,glasses,upper_color,upper_wear"
         options["type"] = attr
@@ -97,8 +149,6 @@ class FaceDetector(Detector):
 
         image = self.get_file_content(path)
         imageType = "BASE64"
-
-        """ 调用人脸检测 """
         self.client.detect(image, imageType)
 
         """ 如果有可选参数 """
@@ -111,9 +161,11 @@ class FaceDetector(Detector):
         """ 带参数调用人脸检测 """
         outcome = self.client.detect(image, imageType, options)
         result = {}
-        result['location'] = outcome['result']['face_list'][0]['location']
-        for items in attributes:
-            result[items] = outcome['result']['face_list'][0][items]
+        if outcome['error_code'] == 0:
+            result = {}
+            # result['location'] = outcome['result']['face_list'][0]['location']
+            for items in attributes:
+                result[items] = outcome['result']['face_list'][0][items]
 
         return result
 
@@ -122,13 +174,17 @@ if __name__ == '__main__':
     try:
         rospy.init_node('name', anonymous=True)
 
+        # k4a = BodyDetector()
+        # result = k4a.detect(['age', 'gender', 'glasses'])
+        # print(result)
+
         face = FaceDetector()
-        result1 = face.detect(attributes=['age', 'gender', 'glasses'])
+        result1 = face.detect(attributes=['age', 'gender', 'glasses', 'beauty'], device='k4a')
 
         body = BodyDetector()
         result2 = body.detect(
             ['age', 'gender', 'upper_wear', 'upper_wear_texture', 'upper_wear_fg', 'upper_color',
-             'lower_wear', 'lower_color', 'face_mask', 'glasses', 'headwear', 'bag'])
+             'lower_wear', 'lower_color', 'face_mask', 'glasses', 'headwear', 'bag'], device='k4a')
 
         print(result1)
         print(result2)

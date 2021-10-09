1e21dad1b0844e0dd149166dfaf8c9371b6b665f
Cmoon <88758720+Cmoon-cyl@users.noreply.github.com>
2021-10-09 10:27:00 +0800

    Add files via upload

--- cmoon/src/Detector.py
+++ cmoon/src/Detector.py
@@ -18,32 +18,9 @@ class Detector(object):
         with open(filePath, 'rb') as fp:
             return fp.read()
 
-    def take_k4a_photo(self):
-        self.modulePath = r'/usr/lib/x86_64-linux-gnu/libk4a.so'
-        self.k4a = pyKinectAzure(self.modulePath)
-        self.k4a.device_open()
-        device_config = self.k4a.config
-        device_config.color_resolution = _k4a.K4A_COLOR_RESOLUTION_1080P
-        print(device_config)
-        self.k4a.device_start_cameras(device_config)
-        path = self.photopath + '/photo.jpg'
-        while True:
-            self.k4a.device_get_capture()
-            color_image_handle = self.k4a.capture_get_color_image()
-            if color_image_handle:
-                color_image = self.k4a.image_convert_to_numpy(color_image_handle)
-                cv2.imwrite(path, color_image)
-                if 'photo.jpg' in os.listdir(self.photopath):
-                    self.k4a.image_release(color_image_handle)
-                    self.k4a.capture_release()
-                    break
-        self.k4a.device_stop_cameras()
-        self.k4a.device_close()
-        return path
-
     def take_photo(self, device='camera'):
         """电脑摄像头拍照保存"""
-        if device == 'k4a':
+        if device == 'k4a' or device == 'kinect':
             self.modulePath = r'/usr/lib/x86_64-linux-gnu/libk4a.so'
             self.k4a = pyKinectAzure(self.modulePath)
             self.k4a.device_open()
@@ -79,13 +56,6 @@ class Detector(object):
     def detect(self, attributes=None, device='camera', *keys):
         """电脑摄像头拍照检测"""
         path = self.take_photo(device)
-        print(path)
-        result = self.get_attr(path, attributes)
-        return result
-
-    def k4a_detect(self, attributes=None, *keys):
-        """电脑摄像头拍照检测"""
-        path = self.take_k4a_photo()
         print(path)
         result = self.get_attr(path, attributes)
         return result
@@ -179,7 +149,7 @@ if __name__ == '__main__':
         # print(result)
 
         face = FaceDetector()
-        result1 = face.detect(attributes=['age', 'gender', 'glasses', 'beauty'], device='k4a')
+        result1 = face.detect(attributes=['age', 'gender', 'glasses', 'beauty','mask'], device='kinect')
 
         body = BodyDetector()
         result2 = body.detect(

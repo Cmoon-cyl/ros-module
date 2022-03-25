4517a13906eb31429f6b010bd3810a1891b48263
Cmoon <88758720+Cmoon-cyl@users.noreply.github.com>
2022-03-25 19:54:09 +0800

    Add files via upload

--- cmoon/src/Detector.py
+++ cmoon/src/Detector.py
@@ -13,13 +13,17 @@ from models.experimental import attempt_load
 from utils.general import non_max_suppression, scale_coords, xyxy2xywh
 from utils.augmentations import letterbox
 from utils.plots import Annotator, colors
+from utils.torch_utils import time_sync
 from pyKinectAzure import pyKinectAzure, _k4a
+from cmoon_msgs.msg import Point
+from base_controller import Base
 
 
 class Detector(object):
     def __init__(self):
+        self.weights = os.path.dirname(__file__) + '/weights/' + 'yolov5s6.pt'
         self.photopath = os.path.dirname(os.path.dirname(__file__)) + '/photo'
-        self.weights = r'/home/cmoon/workingspace/src/cmoon/src/weights/yolov5s6.pt'
+        self.base = Base()
 
     def get_file_content(self, filePath):
         with open(filePath, 'rb') as fp:
@@ -154,31 +158,25 @@ class ObjectDetector(Detector):
         print(self.device)
         self.half = self.device != 'cpu'
         self.imgsz = 640
+        self.t1 = 0.0
+        self.t2 = 0.0
+        self.K = np.array([614.86962890625, 0.0, 635.5834350585938,
+                           0.0, 614.7677612304688, 364.99200439453125,
+                           0.0, 0.0, 1.0]).reshape(3, 3)
+        self.K = np.linalg.inv(self.K)
         self.conf_thres = 0.4
         self.iou_thres = 0.05
-        self.classes = None
-        self.list = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
-                     'traffic light',
-                     'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
-                     'cow',
-                     'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase',
-                     'frisbee',
-                     'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                     'surfboard',
-                     'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana',
-                     'apple',
-                     'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-                     'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
-                     'cell phone',
-                     'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                     'teddy bear',
-                     'hair drier', 'toothbrush']
+        self.classes = [39,41,64,67]
+        #self.classes = None
+        self.list = None
+        self.show = rospy.Publisher('/yolo_result', Point, queue_size=10)
+        self.point = Point()
         super().__init__()
 
     def load_model(self):
         model = attempt_load(self.weights, map_location=self.device)  # load FP32 model
-        # stride = int(model.stride.max())  # model stride
-        # names = model.module.names if hasattr(model, 'module') else model.names  # get class names
+        #stride = int(model.stride.max())  # model stride
+        self.list = model.module.names if hasattr(model, 'module') else model.names  # get class names
         if self.half:
             model.half()  # to FP16
         if self.device != 'cpu':
@@ -197,6 +195,7 @@ class ObjectDetector(Detector):
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if len(img.shape) == 3:
             img = img[None]  # expand for batch dim
+        self.t1 = time_sync()
         return img
 
     def pred(self, model, img0):
@@ -204,7 +203,9 @@ class ObjectDetector(Detector):
         names = model.module.names if hasattr(model, 'module') else model.names  # get class names
         img = self.process_img(stride, img0)
         pred = model(img, augment=False, visualize=False)[0]
+        self.t2 = time_sync()
         pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, self.classes, agnostic=False)
+        result_ = []
         result = []
         name = []
         for i, det in enumerate(pred):
@@ -221,34 +222,66 @@ class ObjectDetector(Detector):
 
                 for *xyxy, conf, cls in reversed(det):
                     xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    # coorindate_cls.append(
+                    # [names[int(cls)], xyxy[:][0].item(), xyxy[:][1].item(), xyxy[:][2].item(), xyxy[:][3].item()])
                     line = (cls, *xywh)  # label format
                     aim = ('%g ' * len(line)).rstrip() % line
                     aim = aim.split(' ')
                     c = int(cls)
                     label = (f'{names[c]} {conf:.2f}')
                     annotator.box_label(xyxy, label, color=colors(c, True))
-                    result.append(aim)
-            if len(result):
-                for classes in result:
+                    result.append([xyxy[:][0].item(), xyxy[:][1].item(), xyxy[:][2].item(), xyxy[:][3].item()])
+                    result_.append(aim)
+            if len(result_):
+                for classes in result_:
                     name.append(self.list[int(classes[0])])
+                    classes[0] = self.list[int(classes[0])]
             img0 = annotator.result()
+            print('time:{:.3f}'.format(self.t2 - self.t1))
             return name, result
 
-    def judge(self, mode, name=None, attributes=None):
+    def transform_(self, n_coorindate_cls, depth_to_color_image):
+        target_coorindate = list()  # 存放目标点三维坐标 二维列表
+        for name, coorindate_cls in n_coorindate_cls:
+            # print("coor:",coorindate_cls)
+            x1, y1, x2, y2 = coorindate_cls[0], coorindate_cls[1], coorindate_cls[2], coorindate_cls[3]
+            center_x = round((x2 - x1) / 2 + x1)
+            center_y = round((y2 - y1) / 2 + y1)
+            # target_coorindate = list()  # 存放目标点三维坐标 二维列表
+            # print("coor:%s,%s" % (center_x, center_y))
+            dep = depth_to_color_image[center_y][center_x].item()
+            if (dep >= 100):  # 大于10cmv
+                # image_coorindate = np.array([center_x, center_y , dep // 2]).reshape(3, 1)  # 图像坐标
+                image_coorindate = np.array([center_x * dep, center_y * dep, dep]).reshape(3, 1)  # 图像坐标
+                camera_coorindate = np.dot(self.K, image_coorindate)  # 相机坐标
+                target_coorindate.append([name, camera_coorindate[0].item() / 1000,
+                                          camera_coorindate[1].item() / 1000,
+                                          camera_coorindate[2].item() / 1000,
+                                          ])
+                self.point.result = name
+                self.point.x = camera_coorindate[0].item() / 1000
+                self.point.y = camera_coorindate[1].item() / 1000
+                self.point.z = camera_coorindate[2].item() / 1000
+                self.show.publish(self.point)
+
+        return target_coorindate
+
+    def judge(self, mode, name=None, find=None):
         if mode == 'realtime':
-            print(name)
             flag = cv2.waitKey(1) & 0xFF == ord('q')
         elif mode == 'find':
-            flag = cv2.waitKey(1) and attributes in name
+            flag = cv2.waitKey(1) and find in name
         else:
             flag = cv2.waitKey(1) and name != []
+            print('Detcet complete.')
         return flag
 
-    def detect(self, attributes=None, device='camera', mode='realtime', *keys):
-        if attributes is not None:
+    def detect(self, find=None, device='camera', mode='realtime', depth=False, rotate=False, save=True,*keys):
+        if find is not None:
             mode = 'find'
         model = self.load_model()
         name = []
+        result = []
         if device == 'k4a' or device == 'kinect':
             self.modulePath = r'/usr/lib/x86_64-linux-gnu/libk4a.so'
             self.k4a = pyKinectAzure(self.modulePath)
@@ -258,16 +291,33 @@ class ObjectDetector(Detector):
             print('Kinect opened!')
             self.k4a.device_start_cameras(device_config)
             while True:
+                if rotate:
+                    self.base.rotate(1.0)
                 self.k4a.device_get_capture()
                 color_image_handle = self.k4a.capture_get_color_image()
+                depth_image_handle = self.k4a.capture_get_depth_image()
+
                 if color_image_handle:
                     img0 = self.k4a.image_convert_to_numpy(color_image_handle)
                     name, result = self.pred(model, img0)
+                    # print(result)
                     cv2.namedWindow('yolo', cv2.WINDOW_NORMAL)
                     cv2.resizeWindow('yolo', 1280, 720)
                     cv2.imshow('yolo', img0)
-                    if self.judge(mode, name, attributes):
+                    if self.judge(mode, name, find):
+                        if save:
+                            cv2.imwrite(self.photopath + '/result.jpg', img0)
                         break
+                    if depth_image_handle and name != list() and depth is True:
+                        depth_color_image = self.k4a.transform_depth_to_color(depth_image_handle, color_image_handle)
+
+                        result = self.transform_(zip(name, result), depth_color_image)
+
+                        print("result:", result)
+                print("result:", result)
+
+            if rotate:
+                self.base.stop()
 
             self.k4a.device_stop_cameras()
             self.k4a.device_close()
@@ -275,18 +325,27 @@ class ObjectDetector(Detector):
         else:
             cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
             cap.open(0)
+            #rate = rospy.Rate(2000)
             while cap.isOpened():
+                if rotate:
+                    #print('rotating')
+                    self.base.rotate(1.0)
+                    #rate.sleep()
                 flag, img0 = cap.read()
                 name, result = self.pred(model, img0)
                 # print(name)
                 cv2.namedWindow('yolo', cv2.WINDOW_NORMAL)
                 cv2.resizeWindow('yolo', 640, 640)
                 cv2.imshow('yolo', img0)
-                if self.judge(mode, name, attributes):
+                if self.judge(mode, name, find):
+                    if save:
+                        cv2.imwrite(self.photopath + '/result.jpg', img0)
                     break
-
+                print("result:", result)
+            if rotate:
+                self.base.stop()
             cap.release()
-        return name
+        return name, result
 
 
 if __name__ == '__main__':
@@ -296,7 +355,7 @@ if __name__ == '__main__':
         # face = FaceDetector()
         # result1 = face.detect(attributes=['age', 'gender', 'glasses', 'beauty', 'mask'], device='cam')
         # print(result1)
-
+        #
         # body = BodyDetector()
         # result2 = body.detect(
         #     ['age', 'gender', 'upper_wear', 'upper_wear_texture', 'upper_wear_fg', 'upper_color',
@@ -304,8 +363,11 @@ if __name__ == '__main__':
         # print(result2)
 
         yolo = ObjectDetector()
-        name = yolo.detect(device='k4a', mode='realtime', attributes='laptop')
-        print('name:{}'.format(name))
+        name, result = yolo.detect(device='cam', mode='realtime', find=None, depth=False, rotate=True)
+        # print(name[0])
+        # print(result)
+        # name = yolo.detect(device='k4a', mode='realtime', attributes=None, depth=True)
+        # print('name:{}'.format(name))
 
     except rospy.ROSInterruptException:
         pass
